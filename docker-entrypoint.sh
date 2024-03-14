d4f58eeffd9918fe78939a033ea77c3692372343
Miłosz B. <milosz.bedrylo@gmail.com>
2024-03-14 15:33:39 +0100

    running now from gunicorn added nginx and certobt, yet to be configured

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 db_password_from_file=`awk 'NR==1' $DB_PASSWORD_FILE`
 export DB_PASSWORD=$db_password_from_file
+python3 manage.py migrate
 if [ $DEBUG == "True" ]; 
 then
-  python3 manage.py migrate && python3 manage.py runserver 0.0.0.0:8000
+   python3 manage.py runserver 0.0.0.0:8000
 else
-  echo "to be implemented"
+  exec gunicorn \
+      --preload \
+      --bind 0.0.0.0:8000 \
+      --name app \
+      --workers 3 \
+      -k uvicorn.workers.UvicornWorker \
+      --forwarded-allow-ips="*" \
+      --log-level=debug \
+      --capture-output --enable-stdio-inheritance \
+      --access-logfile '-' --error-logfile '-' \
+      ligapwrproject.asgi:application
 fi
