7221e3ed81c3c5406a5e9f68831dbf0211a8364d
Marvin Ruciński <marvin@prasa-polska.com>
2024-03-17 12:01:53 +0100

    Minor fixes to docker configs

--- ligapwrproject/settings.py
+++ ligapwrproject/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$jnv$^fc(=2(e_ex5@bqaca9qgryy5w31a&b1!z6n#mc#d5=b'
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
@@ -86,16 +86,6 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", ''),
     }
 }
-
-# "default": {
-#     "ENGINE": "django.db.backends.postgresql",
-#     "NAME": "ligapwr",
-#     "USER": "postgres",
-#     "PASSWORD": "P@ssword",
-#     "HOST": "db",
-#     "PORT": "5432",
-# }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
