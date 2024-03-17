9dc2dee816ae11b9b28e4d5979e871c8174d3df2
Marvin Ruciński <marvin@prasa-polska.com>
2024-03-17 12:15:29 +0100

    Changed domain name

--- ligapwrproject/settings.py
+++ ligapwrproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
