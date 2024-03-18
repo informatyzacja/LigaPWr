cdb8eb86e86acc2f82085a668a366f8ce475cac3
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-03-18 13:29:42 +0100

    fix: remove csrf hosts

--- ligapwrproject/settings.py
+++ ligapwrproject/settings.py
@@ -27,7 +27,6 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "*").split(",")
 
 # Application definition
 
