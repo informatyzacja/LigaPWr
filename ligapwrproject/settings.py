c1d24e1ba41ebf94dc217a001d103da81a1e5ccf
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-03-18 13:41:02 +0100

    fix: default cors origin

--- ligapwrproject/settings.py
+++ ligapwrproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "*").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split(",")
 
 # Application definition
 
